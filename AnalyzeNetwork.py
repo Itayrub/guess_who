35588b10c5b195e1b3429e9a5114e4e9e9db6b63
Itayrub <rubanenko.itay@gmail.com>
2025-12-26 12:36:12 +0200

    pocker - task3

--- AnalyzeNetwork.py
+++ AnalyzeNetwork.py
@@ -2,23 +2,30 @@
 from scapy.all import *
 from mac_vendor_lookup import MacLookup
 
+OS_PING_LOADS = {
+    "Windows": b"abcdefgh",
+    "Linux": b"\x00\x01\x02\x03\x04\x05\x06\x07",
+    "Cisco": b"abcdabcd",
+    "FreeBSD": b" !\"#$%&'",
+    "Solaris": b"\x00\x00\x00\x00\x00\x00\x00\x00"
+}
 class AnalyzeNetwork:
 
     def __device_info__(self, p):
         mac = self.__get_mac__(p)
         vendor = MacLookup().lookup(mac)
         ip = self.__get_ip__(p)
-        ttl = self.__get_ttl__(p)
         
-        dev_info = ({"MAC" : mac, "IP" : ip, "VENDOR" : vendor, "TTL" : ttl })
+        dev_info = ({"MAC" : mac, "IP" : ip, "VENDOR" : vendor, "PACKETS" : [p]})
         dev_info["OS"] = self.guess_os(dev_info)
         return dev_info
 
 
-    def __get_ttl__(self, p):
-        if IP in p:
-            return p[IP].ttl
-        return None
+    def __get_ttl__(self, device):
+        for p in device["PACKETS"]:   
+            if IP in p:
+                return p[IP].ttl
+            return None
     
 
     def __get_ip__(self, p):
@@ -35,6 +42,18 @@ class AnalyzeNetwork:
         
         return None
 
+    def __is_new_device__(self, mac, devices):
+        for dev in devices:
+            if mac == dev["MAC"]:
+                return dev
+        return True
+    
+    def __get_ping_payload__(self, device_info):
+        for p in device_info["PACKETS"]:
+            if p.haslayer(ICMP) and p[ICMP].type == 8 and p.haslayer(Raw):
+                return p[Raw].load
+        return b""
+
 
     def __init__(self, pcap_path):
         """
@@ -44,8 +63,10 @@ class AnalyzeNetwork:
         devices = []
         for p in self.packets:
             dev_info = self.__device_info__(p)
-            if not dev_info in devices:
+            if self.__is_new_device__(dev_info["MAC"], devices):
                 devices.append(dev_info)
+            else:
+                self.__is_new_device__(dev_info["MAC"], devices)["PACKETS"].append(p)
         self.devices = devices
 
 
@@ -85,21 +106,45 @@ class AnalyzeNetwork:
 
     def guess_os(self, device_info):
         """returns assumed os"""
-        ttl = device_info["TTL"]
-        if ttl is None:
-            return None
-        if ttl <= 64:
-            return f"Linux/Unix"
-        elif ttl <= 128:
-            return f"Windows"
-        else:
-            return f"Cisco/Solaris"
+        possible_os = {}
+        #by ttl
+        ttl = self.__get_ttl__(device_info)
+        if not ttl is None:
+            if ttl <= 64:
+                possible_os["Linux"] = 1
+                possible_os["Unix"] = 1
+            elif ttl <= 128:
+                possible_os["Windows"] = 1
+            else:
+                possible_os["Cisco"] = 1
+
+        #by payload
+        load = self.__get_ping_payload__(device_info)
+        for os, fingerprint in OS_PING_LOADS.items():
+            if fingerprint in load:
+                if not os in possible_os:
+                    possible_os[os] = 2
+                else:
+                    possible_os[os] += 2
+        
+
+        possible_os = sorted(possible_os.items(), key=lambda item: item[1], reverse=True)
+        max = possible_os[0][1]
+        return [guess[0] for guess in possible_os if guess[1] == max]
+            
 
 
     def get_info(self):
         """returns a list of dicts with information about every
         device in the pcap"""
-        return self.devices
+        ret = []
+        keys_to_add = ["MAC" ,"IP", "VENDOR", "OS"]
+        for dev in self.devices:
+            info_to_add = {}
+            for key in keys_to_add:
+                info_to_add[key] = dev[key]
+            ret.append(info_to_add)
+        return ret
 
 
     def __repr__(self):
